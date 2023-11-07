d45df3ed03234ac5bbc8de646f4e81d1fb200ca8
Desenvolvimento <airtonluizcanalefilho@hotmail.com@hotmail.com>
2023-11-07 10:00:51 -0300

    feat: comments on posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
 "use client";
 import AddPost from "./components/AddPost";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Post from "./components/Post";
+
+//Fetch all posts
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+
+  if (error) return error;
+  if (isLoading) return "Carregando....";
+
   return (
     <main>
-      <h1>Hello next</h1>
       <AddPost />
+      {data?.map((post) => (
+        <Post
+          key={post.id}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
+          id={post.id}
+        />
+      ))}
     </main>
   );
 }
